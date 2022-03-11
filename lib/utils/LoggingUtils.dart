fec965ed230e40b99422c29c04038a1a58374f8f
Alisson Dias <alisson.dias@niyon.com.br>
2022-03-11 14:48:28 -0300

    Logger implementation

--- lib/utils/LoggingUtils.dart
+++ lib/utils/LoggingUtils.dart
@@ -3,12 +3,23 @@ library dolphin_flutter;
 import 'package:logger/logger.dart';
 
 class LoggingUtils {
-  static var _logger = Logger();
+  static var _logger = Logger(
+    printer: PrettyPrinter()
+  );
 
   static void info(String related, String message) => _logger.i(buildMessage(related, message));
   static void warn(String related, String message) => _logger.w(buildMessage(related, message));
   static void error(String related, String message, [dynamic error, dynamic stackTrace]) => _logger.e(buildMessage(related, message), error, stackTrace);
   static void debug(String related, String message) => _logger.d(buildMessage(related, message));
+
+  static void getXLog(String text, {bool isError = false}) {
+    Future.microtask(() {
+      if(isError)
+        _logger.e(text);
+      else
+        _logger.i(text);
+    });
+  }
 
   static String buildMessage(String? related, String? message) {
     message ??= "Ocorreu um erro, tente novamente!";
