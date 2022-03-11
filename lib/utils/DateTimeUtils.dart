caf77745cc8726b84aa7f3d37bbbf0041ef7796f
Alisson Dias <alisson.dias@niyon.com.br>
2022-03-11 17:00:21 -0300

    Formatters

--- lib/utils/DateTimeUtils.dart
+++ lib/utils/DateTimeUtils.dart
@@ -44,6 +44,8 @@ class DateTimeUtils {
   static String uiFormatBrazil(DateTime date) => DateFormat("dd/MM/yyyy").format(date);
   static DateTime apiFormatDateFromBackend(String date) => DateFormat("dd/MM/yyyy").parse(date);
   static DateTime apiFormatDateTimeFromBackend(String date) => DateFormat("dd/MM/yyyy HH:mm:ss").parse(date);
+  static String apiFormatDateToBackend(DateTime date) => DateFormat("dd/MM/yyyy").format(date);
+  static String apiFormatDateTimeToBackend(DateTime date) => DateFormat("dd/MM/yyyy HH:mm:ss").format(date);
   static String apiFormatInternational(DateTime date) => DateFormat("yyyy-MM-dd").format(date);
   static String apiFormatInternationalWithTime(DateTime date) => DateFormat("yyyy-MM-dd'T'HH:mm:ss'.000Z'").format(date);
 
