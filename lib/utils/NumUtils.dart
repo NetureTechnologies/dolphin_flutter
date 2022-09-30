fb5a4f9a4e1fd3e878dbc38a333abb41eca4beba
Thalles Santos <thalles.rss@gmail.com>
2022-09-30 17:40:16 -0300

    Bytes Converter

--- lib/utils/NumUtils.dart
+++ lib/utils/NumUtils.dart
@@ -6,7 +6,7 @@ class NumUtils {
    * Data Sizes
    */
   static int? bytesToGb(num bytes) {
-    return bytes.convertFromTo(DIGITAL_DATA.byte, DIGITAL_DATA.gigabyte)?.toInt();
+    return bytes.convertFromTo(DIGITAL_DATA.byte, DIGITAL_DATA.gibibyte)?.toInt();
   }
   static int? gbToBytes(num gb) {
     return gb.convertFromTo(DIGITAL_DATA.gibibyte, DIGITAL_DATA.byte)?.toInt();
