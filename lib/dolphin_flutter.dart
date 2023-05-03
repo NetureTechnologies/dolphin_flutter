d11c5707aa3203495c1618ea20d0ce442f36fd3d
Alisson Dias <alisson.dias@niyon.com.br>
2023-05-03 20:32:30 -0300

    Upgrades

--- lib/dolphin_flutter.dart
+++ lib/dolphin_flutter.dart
@@ -4,7 +4,7 @@ import 'package:dolphin_flutter/utils/AppUtils.dart';
 import 'package:dolphin_flutter/utils/DeviceUtils.dart';
 
 class Dolphin {
-  static void initialize() async {
+  static Future<void> initialize() async {
     DeviceUtils.cache();
     await AppUtils.cache();
   }
