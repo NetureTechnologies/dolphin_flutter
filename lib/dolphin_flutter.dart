23c8d55ced6efbabdbcebef4c92ba3e3510e2f83
Alisson Dias <alisson.dias@niyon.com.br>
2023-05-03 20:03:52 -0300

    Upgrades

--- lib/dolphin_flutter.dart
+++ lib/dolphin_flutter.dart
@@ -1,2 +1,9 @@
 library dolphin_flutter;
 
+import 'package:dolphin_flutter/utils/DeviceUtils.dart';
+
+class Dolphin {
+  static void initialize() {
+    DeviceUtils.cache();
+  }
+}
--- lib/utils/DeviceUtils.dart
+++ lib/utils/DeviceUtils.dart
@@ -4,30 +4,9 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 class DeviceUtils {
-  static bool get isPlatformDesktop {
-    try {
-      return Platform.isLinux || Platform.isMacOS || Platform.isWindows;
-    }
-    catch(ex) {
-      return false;
-    }
-  }
-  static bool get isPlatformMobile {
-    try {
-      return Platform.isAndroid || Platform.isIOS || Platform.isFuchsia;
-    }
-    catch(ex) {
-       return false;
-    }
-  }
-  static bool get isPlatformWeb {
-    try {
-      return kIsWeb;
-    }
-    catch(ex) {
-      return false;
-    }
-  }
+  static bool isPlatformDesktop = false;
+  static bool isPlatformMobile = false;
+  static bool isPlatformWeb = false;
 
   static String getOperatingSystem() {
     return Platform.operatingSystem;
@@ -42,5 +21,23 @@ class DeviceUtils {
   // Desktop App only
   static String getHostname() {
     return Platform.localHostname;
+  }
+
+  // Cache
+  static void cache() {
+    try {
+      isPlatformDesktop = Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+    }
+    catch(ex) {}
+
+    try {
+      isPlatformMobile = Platform.isAndroid || Platform.isIOS || Platform.isFuchsia;
+    }
+    catch(ex) {}
+
+    try {
+      isPlatformWeb = kIsWeb;
+    }
+    catch(ex) {}
   }
 }
